9fd1919a4f764956a4d36d4bbfee6ca37221d0de
Rabbi-Choudhury123 <120347465+Rabbi-Choudhury123@users.noreply.github.com>
2022-12-11 20:22:54 -0500

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,15 @@
+print("Simple Claculator")
+x = 10
+y = 20
+
+sum = x + y
+difference = x - y
+product = x * y
+quotiant = x/y
+remainder = x % y
+
+print("The sum of x and y is ", sum)
+print("The Differnce between x and y is ", difference)
+print("The prosuct of x and y is ", product)
+print("The quotiant of and y is ", quotiant)
+print("The reaminder of x and y is ", remainder)
